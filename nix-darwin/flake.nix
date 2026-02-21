ea44d6d1bc9de8a125cad7915d28618d4c49fdeb
anyoussef <tonerbtw@gmail.com>
2026-02-20 19:49:21 -0600

    nvim working config along with dependencies. todo: refactor into modules

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -12,7 +12,6 @@
 
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
 
-    # Declarative Homebrew taps
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
       flake = false;
@@ -24,146 +23,174 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    darwin,
-    home-manager,
-    nix-homebrew,
-    homebrew-core,
-    homebrew-cask,
-    ...
-  }: let
-    system = "aarch64-darwin";
-  in {
-    darwinConfigurations.fartbox = darwin.lib.darwinSystem {
-      inherit system;
+  outputs =
+    { self
+    , nixpkgs
+    , darwin
+    , home-manager
+    , nix-homebrew
+    , homebrew-core
+    , homebrew-cask
+    , ...
+    }:
+    let
+      system = "aarch64-darwin";
+    in
+    {
+      darwinConfigurations.fartbox = darwin.lib.darwinSystem {
+        inherit system;
 
-      modules = [
-        # ----------------------------
-        # nix-homebrew
-        # ----------------------------
-        nix-homebrew.darwinModules.nix-homebrew
-        {
-          nix-homebrew = {
-            enable = true;
-            enableRosetta = true;
-            user = "ayous";
+        modules = [
+          # ----------------------------
+          # nix-homebrew (installs brew itself)
+          # ----------------------------
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              enable = true;
+              enableRosetta = true;
+              user = "ayous";
 
-            taps = {
-              "homebrew/homebrew-core" = homebrew-core;
-              "homebrew/homebrew-cask" = homebrew-cask;
+              taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+              };
+
+              mutableTaps = false;
+            };
+          }
+
+          # ----------------------------
+          # System Configuration
+          # ----------------------------
+          {
+            nixpkgs.hostPlatform = system;
+            nixpkgs.config.allowUnfree = true;
+
+            system.primaryUser = "ayous";
+            system.stateVersion = 4;
+
+            nix.settings.experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+
+            programs.zsh.enable = true;
+
+            environment.systemPackages = with nixpkgs.legacyPackages.${system}; [
+              coreutils
+            ];
+
+            fonts.packages = with nixpkgs.legacyPackages.${system}; [
+              nerd-fonts.jetbrains-mono
+            ];
+
+            users.users.ayous.home = "/Users/ayous";
+            ids.gids.nixbld = 350;
+
+            # ----------------------------
+            # Declarative Homebrew
+            # ----------------------------
+            homebrew = {
+              enable = true;
+
+              onActivation = {
+                autoUpdate = false;
+                upgrade = false;
+                cleanup = "uninstall";
+              };
+
+              taps = [
+                "homebrew/homebrew-core"
+                "homebrew/homebrew-cask"
+              ];
+
+              brews = [
+              ];
+
+              casks = [
+                "visual-studio-code"
+                "discord"
+                "anaconda"
+                "onedrive"
+                "microsoft-outlook"
+                "blender"
+                "minecraft"
+              ];
             };
 
-            mutableTaps = false;
-          };
-        }
-
-        # ----------------------------
-        # System Configuration
-        # ----------------------------
-        {
-          nixpkgs.hostPlatform = system;
-          nixpkgs.config.allowUnfree = true;
-
-          system.primaryUser = "ayous";
-          system.stateVersion = 4;
-
-          nix.settings.experimental-features = [
-            "nix-command"
-            "flakes"
-          ];
-
-          programs.zsh.enable = true;
-
-          environment.shells = with nixpkgs.legacyPackages.${system}; [
-            bash
-            zsh
-          ];
-
-          environment.systemPackages = with nixpkgs.legacyPackages.${system}; [
-            coreutils
-          ];
-
-          fonts.packages = with nixpkgs.legacyPackages.${system}; [
-            nerd-fonts.jetbrains-mono
-          ];
-
-          users.users.ayous = {
-            home = "/Users/ayous";
-          };
-
-          ids.gids.nixbld = 350;
-
-          system.defaults.dock = {
-            autohide = true;
-            show-recents = false;
-            persistent-apps = [
-              "/System/Applications/Messages.app"
-              "/System/Applications/Mail.app"
-              "/System/Applications/System Settings.app"
-              "/Applications/Nix Apps/Firefox Developer Edition.app"
-              "/Applications/Nix Apps/iTerm2.app"
-            ];
-          };
-        }
-
-        # ----------------------------
-        # Home Manager
-        # ----------------------------
-        home-manager.darwinModules.home-manager
-
-        {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-
-            users.ayous = {pkgs, ...}: {
-              home.username = "ayous";
-              home.homeDirectory = "/Users/ayous";
-              home.stateVersion = "22.11";
-
-              # ------------------------
-              # Packages
-              # ------------------------
-              home.packages = with pkgs; [
-                # Apps
-                raycast
-                iterm2
-                firefox-devedition
-                spotify
-
-                # CLI
-                git
-                neovim
-                ripgrep
-                fzf
-                lua
-                curl
-                tree-sitter
+            # ----------------------------
+            # macOS Defaults
+            # ----------------------------
+            system.defaults.dock = {
+              autohide = true;
+              show-recents = false;
+              persistent-apps = [
+                "/Users/ayous/Applications/Home Manager Apps/Firefox Developer Edition.app"
+                "/System/Applications/Messages.app"
+                "/Users/ayous/Applications/Home Manager Apps/Obsidian.app"
+                "/Applications/Discord.app"
+                "/Applications/Blender.app"
+                "/Applications/Visual Studio Code.app"
+                "/System/Applications/Mail.app"
+                "/Applications/Microsoft Outlook.app"
+                "/System/Applications/Calendar.app"
+                "/Applications/Minecraft.app"
+                "/Users/ayous/Applications/Home Manager Apps/Iterm2.app"
+                "/System/Applications/System Settings.app"
               ];
+            };
+          }
 
-              home.sessionPath = [
-                "$HOME/.nix-profile/bin"
-                "/run/current-system/sw/bin"
-              ];
+          # ----------------------------
+          # Home Manager
+          # ----------------------------
+          home-manager.darwinModules.home-manager
 
-              # ------------------------
-              # Zsh
-              # ------------------------
-              programs.zsh = {
-                enable = true;
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
 
-                oh-my-zsh = {
+              users.ayous = { pkgs, ... }: {
+                home.username = "ayous";
+                home.homeDirectory = "/Users/ayous";
+                home.stateVersion = "22.11";
+
+                home.packages = with pkgs; [
+                  git
+                  cargo
+                  fd
+                  scala_2_13
+                  neovim
+                  ripgrep
+                  fzf
+                  lua
+                  wget
+                  luarocks
+                  curl
+                  tree-sitter
+                  spotify
+                  firefox-devedition
+                  raycast
+                  iterm2
+                  obsidian
+                  lazygit
+                ];
+
+                programs.zsh = {
                   enable = true;
-                  theme = "gozilla";
-                  plugins = ["git"];
+
+                  oh-my-zsh = {
+                    enable = true;
+                    theme = "gozilla";
+                    plugins = [ "git" ];
+                  };
                 };
               };
             };
-          };
-        }
-      ];
+          }
+        ];
+      };
     };
-  };
 }
